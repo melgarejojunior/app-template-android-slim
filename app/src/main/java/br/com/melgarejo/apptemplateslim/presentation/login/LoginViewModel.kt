3b444e82ff977bd846f9fc03fcfb105989e4a72b
Junior Melgarejo <juniormelgarejo@jera.com.br>
2019-01-12 18:45:11 -0300

    Scheduler

--- app/src/main/java/br/com/melgarejo/apptemplateslim/presentation/login/LoginViewModel.kt
+++ app/src/main/java/br/com/melgarejo/apptemplateslim/presentation/login/LoginViewModel.kt
@@ -1,9 +1,11 @@
 package br.com.melgarejo.apptemplateslim.presentation.login
 
+import br.com.melgarejo.apptemplateslim.domain.boundary.resources.SchedulerProvider
 import br.com.melgarejo.apptemplateslim.domain.interactor.user.SignIn
 import br.com.melgarejo.apptemplateslim.presentation.structure.base.BaseViewModel
 
 class LoginViewModel(
-    private val signIn: SignIn
+    private val signIn: SignIn,
+    private val schedulerProvider: SchedulerProvider
 ) : BaseViewModel() {
 }
